589bb928b2cf6680599cea63748d055e5fa97e88
harshals21 <61793912+harshals21@users.noreply.github.com>
2023-07-09 21:27:45 +0530

    Add: project folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import { useState } from 'react';
 
 function App() {
+  const [generatedExcuse, setGeneratedExcuse] = useState("");
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
+      (res) => {
+        setGeneratedExcuse(res.data[0].excuse);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='heading'> Generate An Excuse </h1>
+      <button className='btn' onClick={() => fetchExcuse("party")}> Party </button>
+      <button className='btn' onClick={() => fetchExcuse("family")}> Family </button>
+      <button className='btn' onClick={() => fetchExcuse("office")}> Office </button>
+
+      <p className='para'> {generatedExcuse} </p>
     </div>
   );
 }
